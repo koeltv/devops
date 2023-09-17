7c31564bd6ec7168dc676b73bffd9de1e842ac52
koeltv <Valentin.koeltgen@utt.fr>
2023-09-17 11:44:49 +0300

    Changed service2 docker build to use alpine-like base image and use custom JRE, shrinking image size from 181.58 MB to 86.19 MB

--- service2/build.gradle.kts
+++ service2/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("INACCESSIBLE_TYPE")
+
 val ktorVersion: String by project
 val kotlinVersion: String by project
 val logbackVersion: String by project
@@ -5,6 +7,7 @@ val logbackVersion: String by project
 plugins {
     kotlin("jvm") version "1.9.10"
     id("io.ktor.plugin") version "2.3.4"
+    id("org.beryx.runtime") version "1.13.0"
 }
 
 group = "com.koeltv"
@@ -17,6 +20,10 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 repositories {
     mavenCentral()
 }
@@ -27,4 +34,23 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
+
+val jreVersion: String by project
+val downloadPage: String by project
+val baseModules: String by project
+val targets: String by project
+
+runtime {
+    options.set(listOf("--strip-debug", "--compress", "1", "--no-header-files", "--no-man-pages"))
+    modules.set(baseModules.split(','))
+    targets.split(',').run {
+        filter { target -> project.hasProperty(target) || none { project.hasProperty(it) } }
+            .forEach {
+                val format = if (it == "windows") "zip" else "tar.gz"
+                val encodedJreVersion = jreVersion.replace("_", "%2B")
+                val link = "$downloadPage/jdk-$encodedJreVersion/OpenJDK17U-jdk_x64_${it}_hotspot_$jreVersion.$format"
+                targetPlatform(it) { setJdkHome(jdkDownload(link)) }
+            }
+    }
 }
