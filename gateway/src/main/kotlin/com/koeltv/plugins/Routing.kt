eeafdbc6d1ba39036906d77f2e77762a0c16facd
koeltv <Valentin.koeltgen@utt.fr>
2023-11-21 23:07:22 +0200

    First implementation of /mqstatistic route

--- gateway/src/main/kotlin/com/koeltv/plugins/Routing.kt
+++ gateway/src/main/kotlin/com/koeltv/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.koeltv.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -7,6 +8,12 @@ fun Application.configureRouting() {
     routing {
         get("/messages") {
             call.proxyTo("http://monitor:8080")
+        }
+        get("/mqstatistic") {
+            call.proxyTo("http://broker:15672/api/overview") {
+                method = HttpMethod.Get
+                headers[HttpHeaders.Authorization] = "Basic Z3Vlc3Q6Z3Vlc3Q="
+            }
         }
     }
 }
